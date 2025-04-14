3facc68504ff324e030a9ac94303f1276c88a678
Nastaran Adeli <adeli.nastaran@gmail.com>
2025-04-14 00:15:34 -0400

    seriously

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -38,7 +38,8 @@ mkdir user_logs
 cd processed
 mkdir event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cp ./data/raw/rawdata/*server*.log ./data/processed/server_logs
+cd ./data/raw/rawdata
+cp *server*.log ./data/processed/server_logs
 # 6. Repeat the above step for user logs and event logs
 cp ./data/raw/rawdata/*user*.log .data/processed/user_logs
 cp ./data/raw/rawdata/*event*.log .data/processed/event_logs
