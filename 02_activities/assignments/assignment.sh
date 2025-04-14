4b790daa8683988656c0dd91ecf8dcc6114b9cd9
Nastaran Adeli <adeli.nastaran@gmail.com>
2025-04-13 22:04:39 -0400

    4th try to fix errors

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -36,10 +36,10 @@ mkdir server_logs
 mkdir user_logs 
 mkdir event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cp ./data/raw/rawdata/*server*.log processed/server_logs
+cp ./data/raw/*server*.log processed/server_logs
 # 6. Repeat the above step for user logs and event logs
-cp ./data/raw/rawdata/*user*.log processed/user_logs
-cp ./data/raw/rawdata/*event*.log processed/event_logs
+cp ./data/raw/*user*.log processed/user_logs
+cp ./data/raw/*event*.log processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/rawdata/*ipaddr*.*
 rm ./data/processed/user_logs/*ipaddr*.* 
