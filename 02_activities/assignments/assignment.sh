29c05c26f73016f1197f4eebcc02f9c06859b382
Nastaran Adeli <adeli.nastaran@gmail.com>
2025-04-13 21:59:01 -0400

    3rd update to fix errors

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -36,15 +36,13 @@ mkdir server_logs
 mkdir user_logs 
 mkdir event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cp raw/rawdata/*server*.log processed/server_logs
+cp ./data/raw/rawdata/*server*.log processed/server_logs
 # 6. Repeat the above step for user logs and event logs
-cp raw/rawdata/*user*.log processed/user_logs
-cp raw/rawdata/*event*.log processed/event_logs
+cp ./data/raw/rawdata/*user*.log processed/user_logs
+cp ./data/raw/rawdata/*event*.log processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-cd data/raw/rawdata/
-rm *ipaddr*.* 
-cd data/processed/user_logs
-rm *ipaddr*.* 
+rm ./data/raw/rawdata/*ipaddr*.*
+rm ./data/processed/user_logs/*ipaddr*.* 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 cd data
 touch inventory.txt
